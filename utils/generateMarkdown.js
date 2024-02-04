a9fd24240640abf638b311d0e51082704b116f0e
6nhuseyin <6nhuseyin@gmail.com>
2024-02-04 22:58:26 +0000

    add generate markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let markdown = `# ${data.title}\n\n`;
+  markdown += `## Description\n${data.description}\n\n`;
 
-`;
+
+  markdown += `## Installation\n${data.install}\n\n`;
+  markdown += `## Usage\n${data.usage}\n\n`;
+  markdown += `## License\nThis project is licensed under the ${data.license} license.\n\n`;
+  markdown += `## Contributing\n${data.contribution}\n\n`;
+  markdown += `## Tests\n${data.tests}\n\n`;
+  markdown += `## Questions\nFor any questions, please contact me at [${data.email}](mailto:${data.email}).\n`;
+  markdown += `## Table of Contents\n`;
+  markdown += `- [Description](#description)\n`;
+  markdown += `- [Installation](#installation)\n`;
+  markdown += `- [Usage](#usage)\n`;
+  markdown += `- [License](#license)\n`;
+  markdown += `- [Contributing](#contributing)\n`;
+  markdown += `- [Tests](#tests)\n`;
+  markdown += `- [Questions](#questions)\n\n`;
+  return markdown;
+
 }
 
 module.exports = generateMarkdown;
